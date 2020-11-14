70a3664448d54618c878aea636b46a54d87ddfbb
Mehmet Nadi <mehmetnadi@Mehmets-MBP.lan>
2020-11-13 20:06:49 -0800

    worked on local storege with omer

--- src/components/PreviewCard.jsx
+++ src/components/PreviewCard.jsx
@@ -1,18 +1,53 @@
-import React from 'react'
-import './PreviewCard.css';
-import {Link} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import "./PreviewCard.css";
+import { Link } from "react-router-dom";
 
 function PreviewCard(props) {
   return (
     <div className="PreviewCard">
       <div className="previewCard-contanier">
-    
-          <Link to={`/moviedetail/${props.id}`} style={{ textDecoration: 'none', color: "black" }}><img src={`https://image.tmdb.org/t/p/original/${props.imgPath}`} alt="movie-poster"/></Link>
-          <Link to={`/moviedetail/${props.id}`} style={{ textDecoration: 'none', color: "black" }}><h3 className="previewCard-title">{props.name}</h3></Link>
-          <h6>{props.overview}</h6>
+        <Link
+          to={`/moviedetail/${props.id}`}
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <img
+            src={`https://image.tmdb.org/t/p/original/${props.imgPath}`}
+            alt="movie-poster"
+          />
+        </Link>
+        <Link
+          to={`/moviedetail/${props.id}`}
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <h3 className="previewCard-title">{props.name}</h3>
+        </Link>
+
+        {props.isLiked ? (
+          <button
+            style={{ color: "red" }}
+            className="Unlike-btn"
+            onClick={() => {
+              props.onUnliked(props.id);
+            }}
+          >
+            Unlike
+          </button>
+        ) : (
+          <button
+            style={{ color: "green" }}
+            className="like-btn"
+            onClick={() => {
+              props.onLiked(props.id);
+            }}
+          >
+            Like
+          </button>
+        )}
+
+        <h6>{props.overview}</h6>
       </div>
       <div className="rating-box">
-        <i style={{fontStyle: "normal"}}>IMDB: {props.ratingScore}</i>
+        <i style={{ fontStyle: "normal" }}>IMDB: {props.ratingScore}</i>
       </div>
     </div>
   );
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,73 +1,114 @@
-import React, {useState} from 'react'
-import './SearchPage.css';
-import InputGroup from 'react-bootstrap/InputGroup'
-import Button from 'react-bootstrap/Button'
-import FormControl from 'react-bootstrap/FormControl'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import PreviewCard from '../components/PreviewCard'
-import Navbar from '../components/Navbar'
+import React, { useState, useEffect } from "react";
+import "./SearchPage.css";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
+import FormControl from "react-bootstrap/FormControl";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PreviewCard from "../components/PreviewCard";
+import Navbar from "../components/Navbar";
 function SearchPage() {
-
   //API to request --> https://api.themoviedb.org/3/search/movie?api_key=f3e744226b13ef39764f0e35686bff5e&language=en-US&query=Batman&page=1&include_adult=false
   //Handle getting data from API
-  
+
   async function fetchItems() {
-    const data = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=f3e744226b13ef39764f0e35686bff5e&language=en-US&query=${userInput}&page=1&include_adult=false`);
+    const data = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=f3e744226b13ef39764f0e35686bff5e&language=en-US&query=${userInput}&page=1&include_adult=false`
+    );
     const itemsData = await data.json();
-    console.log(itemsData.results)
-    setSearchResult(itemsData.results)
+    console.log(itemsData.results);
+    setSearchResult(itemsData.results);
   }
-  //Hold the Search result 
-  const [searchResult, setSearchResult] = useState([])
+  //Hold the Search result
+  const [searchResult, setSearchResult] = useState([]);
 
   //User input
-  const [userInput, setUserInput] = useState('');
-  
+  const [userInput, setUserInput] = useState("");
+
   //Handle User typing
-  const HandleUserTyping = event => {
+  const HandleUserTyping = (event) => {
     setUserInput(event.target.value);
-    if (userInput !== '') {
+    if (userInput !== "") {
       fetchItems();
     } else {
       setSearchResult([]);
     }
 
-    console.log(userInput)
-  }
+    console.log(userInput);
+  };
 
-    return (
+  //Handle keep liked movies
+  const [likedMovies, setLikedMovies] = useState([]);
+
+  useEffect(() => {
+    const likedMovesString = localStorage.getItem("likedMovies");
+    if (likedMovesString) {
+      const likedMovies = JSON.parse(likedMovesString);
+      setLikedMovies(likedMovies);
+    } else {
+      localStorage.setItem("likedMovies", "[]");
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("likedMovies", JSON.stringify(likedMovies));
+  }, [likedMovies]);
+
+  const handleLikedMovie = (id) => {
+    setLikedMovies((previousLikedMovies) => {
+      return [...previousLikedMovies, id];
+    });
+  };
+
+  const handleUnlikedMovie = (id) => {
+    setLikedMovies((previousLikedMovies) => {
+      return previousLikedMovies.filter((movieID) => movieID !== id);
+    });
+  };
+
+  return (
     <div className="SearchPage">
       <Navbar />
       <div className="search-container">
-          <div className="search-box">
-            
-            <InputGroup className="mb-3">
-              <FormControl
-                placeholder="Search Movie"
-                aria-label="Search Movie"
-                aria-describedby="basic-addon2"
-                value = {userInput}
-                onChange={HandleUserTyping}
-              />
-              <InputGroup.Append>
-                <Button onClick={()=> {
-                  setUserInput('');
-                  setSearchResult([])
-                  }} variant="dark">Clear</Button>
-              </InputGroup.Append>
-            </InputGroup>
-          </div>
+        <div className="search-box">
+          <InputGroup className="mb-3">
+            <FormControl
+              placeholder="Search Movie"
+              aria-label="Search Movie"
+              aria-describedby="basic-addon2"
+              value={userInput}
+              onChange={HandleUserTyping}
+            />
+            <InputGroup.Append>
+              <Button
+                onClick={() => {
+                  setUserInput("");
+                  setSearchResult([]);
+                }}
+                variant="dark"
+              >
+                Clear
+              </Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </div>
       </div>
 
       <div className="preview-card-wrapper">
-        
-
-        {searchResult.map(item => {
-          return <PreviewCard key={item.id} id={item.id} name={item.original_title} overview={item.overview} imgPath={item.poster_path} ratingScore={item.vote_average} /> 
-      
+        {searchResult.map((item) => {
+          return (
+            <PreviewCard
+              key={item.id}
+              id={item.id}
+              isLiked={likedMovies.includes(item.id)}
+              onUnliked={handleUnlikedMovie}
+              onLiked={handleLikedMovie}
+              name={item.original_title}
+              overview={item.overview}
+              imgPath={item.poster_path}
+              ratingScore={item.vote_average}
+            />
+          );
         })}
-
-      
       </div>
     </div>
   );
